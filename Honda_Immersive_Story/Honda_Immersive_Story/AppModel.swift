83d1b833e7dc3d710009146cab067948592829c2
messitu10 <chuanzht@andrew.cmu.edu>
2025-02-03 13:51:26 -0500

    Initial Setup

---
+++ Honda_Immersive_Story/Honda_Immersive_Story/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Honda_Immersive_Story
+//
+//  Created by messitu on 2/3/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Honda_Immersive_Story/Honda_Immersive_Story/Honda_Immersive_StoryApp.swift
@@ -0,0 +1,33 @@
+//
+//  Honda_Immersive_StoryApp.swift
+//  Honda_Immersive_Story
+//
+//  Created by messitu on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Honda_Immersive_StoryApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
